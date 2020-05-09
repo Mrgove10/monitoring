0ac580fbbba7ec4cd8c88d124ea3555aa03b7475
Adrien RICHARD <richardadrien0@gmail.com>
2020-05-09 23:18:25 +0200

    renamed

---
+++ dashboard-app/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeRoutingModule } from './home/home-routing.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    HomeRoutingModule,
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
